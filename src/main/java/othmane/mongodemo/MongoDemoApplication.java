db9c7dcaf97aa161e0d7927c88a54b8c59409b19
gripexdev <cj.othman@gmail.com>
2024-09-13 18:05:31 +0100

    setup project

---
+++ src/main/java/othmane/mongodemo/MongoDemoApplication.java
@@ -0,0 +1,13 @@
+package othmane.mongodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDemoApplication.class, args);
+	}
+
+}
